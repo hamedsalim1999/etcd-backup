b7811a704b87335c56889e4bc2407b22e54189e1
hamedsalim <hamediran1999@gmail.com>
2021-05-02 19:19:40 +0430

    fix some bugs

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ import json
 import os
 from fastapi import FastAPI
 etcd_ip = os.environ.get('ETCD_IP_ADDR')
-etcd_port =int(os.environ.get('ETCD_PORT')
+etcd_port =int(os.environ.get('ETCD_PORT'))
 etcd = etcd3.client(host=os.environ.get('ETCD_IP_ADDR'), port=int(os.environ.get('ETCD_PORT')))
 def get_data_from_etcd(etcd_ip,etcd_port):
     etcd = etcd3.client(host=etcd_ip, port=etcd_port)
     json_dic ={}
     for value, metadata in etcd.get_all():
         json_dic.update({metadata.key.decode('utf-8'):value.decode('utf-8')})
-    return json_object=json.dumps(json_dic)
+    return json.dumps(json_dic)
 
 app = FastAPI()
 @app.get('/')
