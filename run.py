e33e6e561d25a2ea0b0a2d486c6df68cd20ab1d1
hamedsalim <hamediran1999@gmail.com>
2021-05-02 19:25:50 +0430

    remove int type

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import json
 import os
 from fastapi import FastAPI
 etcd_ip = os.environ.get('ETCD_IP_ADDR')
-etcd_port =int(os.environ.get('ETCD_PORT'))
+etcd_port =os.environ.get('ETCD_PORT')
 etcd = etcd3.client(host=os.environ.get('ETCD_IP_ADDR'), port=int(os.environ.get('ETCD_PORT')))
 def get_data_from_etcd(etcd_ip,etcd_port):
     etcd = etcd3.client(host=etcd_ip, port=etcd_port)
