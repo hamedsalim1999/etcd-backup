3585ae3f727f1f6ed926dd0de9757098813bc80d
hamedsalim <hamediran1999@gmail.com>
2021-05-02 19:05:08 +0430

    add function for exract all etcd data

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import etcd3
 import json
-etcd = etcd3.client(host='127.0.0.1', port=2379)
+import os
+etcd_ip = os.environ.get('ETCD_IP_ADDR')
+etcd_port =int(os.environ.get('ETCD_PORT')
+etcd = etcd3.client(host=os.environ.get('ETCD_IP_ADDR'), port=int(os.environ.get('ETCD_PORT')))
+def get_data_from_etcd(etcd_ip,etcd_port):
+    etcd = etcd3.client(host=etcd_ip, port=etcd_port)
+    json_dic ={}
+    for value, metadata in etcd.get_all():
+        json_dic.update({metadata.key.decode('utf-8'):value.decode('utf-8')})
+    return json_object=json.dumps(json_dic)
 
-json_dic ={}
-for value, metadata in etcd.get_all():
-    json_dic.update({metadata.key.decode('utf-8'):value.decode('utf-8')})
-
-json_object=json.dumps(json_dic)
 print(json_object)
 
 
