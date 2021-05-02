f69eae38a321b9748e876b558ecd2357d03f2381
hamedsalim <hamediran1999@gmail.com>
2021-05-02 19:50:18 +0430

    can read etc env from env file

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ import os
 from fastapi import FastAPI
 etcd_ip = os.environ.get('ETCD_IP_ADDR')
 etcd_port =os.environ.get('ETCD_PORT')
-# etcd = etcd3.client(host=os.environ.get('ETCD_IP_ADDR'), port=int(os.environ.get('ETCD_PORT')))
-def get_data_from_etcd():
-    etcd = etcd3.client(host='127.0.0.1', port=2379)
+etcd_ip = int(etcd_ip)
+def get_data_from_etcd(etcd_port, etcd_ip):
+    etcd = etcd3.client(etcd_port, etcd_ip)
     json_dic ={}
     for value, metadata in etcd.get_all():
         json_dic.update({metadata.key.decode('utf-8'):value.decode('utf-8')})
@@ -15,7 +15,7 @@ def get_data_from_etcd():
 app = FastAPI()
 @app.get('/')
 async def send_data():
-    return get_data_from_etcd(get_data_from_etcd())
+    return get_data_from_etcd(etcd_ip,etcd_port)
 
 
 
