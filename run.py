b2119cb65493d46cf275b09b3e85bf564b77cfd2
hamedsalim <hamediran1999@gmail.com>
2021-05-02 18:59:19 +0430

    add json convet on app

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-import etcd
+import etcd3
+import json
+etcd = etcd3.client(host='127.0.0.1', port=2379)
 
-client = etcd.Client(host='127.0.0.1', port=2379)
+json_dic ={}
+for value, metadata in etcd.get_all():
+    json_dic.update({metadata.key.decode('utf-8'):value.decode('utf-8')})
+
+json_object=json.dumps(json_dic)
+print(json_object)
+
+
