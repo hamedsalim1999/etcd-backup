3a3e588fe5601883ee2a47d57a1b99e53ba46ffa
hamedsalim <hamediran1999@gmail.com>
2021-05-09 22:19:03 +0430

    write test for this app

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from run import sqlcon, etcd_data_to_sql,etcd_data_to_json
+
+class TestDb(unittest.TestCase):
+    def setUp(self):
+        self.db = sqlcon('test')
+        self.obj = {'key1':'val1','key2':'val2','key3':'val3','key4':'val5'}
+    def etcd_data_to_sql(self):
+        etcd_data_to_sql(self.obj,self.db)
+        user = self.db.query.filter_by(value='val1')
+        assert user is not None
