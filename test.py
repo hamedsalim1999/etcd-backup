8f9fa177be58d76870a0bb13a6f81383f78884e1
hamedsalim <hamediran1999@gmail.com>
2021-05-09 23:15:05 +0430

    change sql query

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class TestDb(unittest.TestCase):
         self.obj = etcd_connect('127.0.0.1','2379')
     def test_etcd_data_to_sql(self):
         etcd_data_to_sql(self.obj,self.db)
-        user = self.db.query.filter_by(value='val1')
+        user = self.db.execute("SELECT key FROM etcd")
         self.assertFalse( user , None)
 if __name__ == '__main__':
     unittest.main()
