7b08234bdca610cd7e9a9d5a30f39297a990f9df
hamedsalim <hamediran1999@gmail.com>
2021-05-09 22:45:20 +0430

    add test_ key world

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class TestDb(unittest.TestCase):
     def setUp(self):
         self.db = sqlcon('test')
         self.obj = {'key1':'val1','key2':'val2','key3':'val3','key4':'val5'}
-    def etcd_data_to_sql(self):
+    def test_etcd_data_to_sql(self):
         etcd_data_to_sql(self.obj,self.db)
         user = self.db.query.filter_by(value='val1')
         self.assertFalse( user , None)
