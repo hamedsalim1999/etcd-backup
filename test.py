842e1db37e67103f2ad51656148021eea3e7b7d1
hamedsalim <hamediran1999@gmail.com>
2021-05-09 23:40:02 +0430

    add color for output

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import Mock
 from run import sqlcon, etcd_data_to_sql,etcd_data_to_json,etcd_connect
+from colorama import Fore, Back, Style
 class TestDb(unittest.TestCase):
     def setUp(self):
         self.db = sqlcon('test')
@@ -10,14 +11,14 @@ class TestDb(unittest.TestCase):
         try:
             etcd_data_to_sql(self.obj,self.db)
             self.db.execute("SELECT * from etcd")
-            print(f"Test etcd_data_to_sql Successfully!")
+            print(Back.GREEN+"Test etcd_data_to_sql Successfully!")
         except:
-            print(f"Test etcd_data_to_sql FAIL")
+            print(Fore.RED +"Test etcd_data_to_sql FAIL")
     def test_etcd_data_to_json(self):
         try:
             etcd_data_to_json(self.obj,'data')
-            print(f"Test test_etcd_data_to_json Successfully!")
+            print(Back.GREEN+"Test test_etcd_data_to_json Successfully!")
         except:
-            print(f"Test test_etcd_data_to_json FAIL")
+            print(Fore.RED +"Test test_etcd_data_to_json FAIL")
 if __name__ == '__main__':
     unittest.main()
