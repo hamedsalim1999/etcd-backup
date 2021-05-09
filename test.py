064854a4f81a5ea4f64a606ea281088d192a0dcd
hamedsalim <hamediran1999@gmail.com>
2021-05-09 22:51:41 +0430

    add items for ittrate

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from run import sqlcon, etcd_data_to_sql,etcd_data_to_json
 class TestDb(unittest.TestCase):
     def setUp(self):
         self.db = sqlcon('test')
-        self.obj = {'key1':'val1','key2':'val2','key3':'val3','key4':'val5'}
+        self.obj = {'key1':'val1','key2':'val2','key3':'val3','key4':'val5'}.items()
     def test_etcd_data_to_sql(self):
         etcd_data_to_sql(self.obj,self.db)
         user = self.db.query.filter_by(value='val1')
