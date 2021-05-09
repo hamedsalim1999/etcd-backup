78d0eb984a333748eb46b628a2566507d00f9b14
hamedsalim <hamediran1999@gmail.com>
2021-05-09 23:26:15 +0430

    add color for

--- test.py
+++ test.py
@@ -6,8 +6,13 @@ class TestDb(unittest.TestCase):
         self.db = sqlcon('test')
         self.obj = etcd_connect('127.0.0.1','2379')
     def test_etcd_data_to_sql(self):
-        etcd_data_to_sql(self.obj,self.db)
-        user = self.db.execute("SELECT * from etcd")
-        # self.assertFalse( user , None)
+        try:
+            etcd_data_to_sql(self.obj,self.db)
+            user = self.db.execute("SELECT * from etcd")
+            print(f"{'\033[92m'}Test etcd_data_to_sql Successfully!{'\033[92m'}")
+        except:
+            print(f"{'\033[91m'}Test etcd_data_to_sql FAIL{'\033[91m'}")
+    def test_etcd_data_to_json(self):
+        pass
 if __name__ == '__main__':
     unittest.main()
